d20feb91daa1ede528f4c52fef8a5bac364df4fd
gitprojects <arunkumarm.22aid@kongu.edu>
2025-02-15 16:11:54 +0530

    custom login page

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './pages/LoginPage';
+import RegisterPage from './pages/RegisterPage';
+export default function App(){
+  return(
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Routes>
+    </Router>
+  );
+
+}
